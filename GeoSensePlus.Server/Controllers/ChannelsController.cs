c3ecd5472d7461f201503d782dfb7158e743a4eb
Ron <li.rongcheng@gmail.com>
2020-08-26 00:55:23 +1000

    implement ChannelController

--- GeoSensePlus.Server/Controllers/ChannelsController.cs
+++ GeoSensePlus.Server/Controllers/ChannelsController.cs
@@ -1,55 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using GeoSensePlus.Mongo;
+using GeoSensePlus.Server.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreUtils.Database.MongoDb;
 
 namespace GeoSensePlus.Server.Controllers
 {
-    public class MongoController<TDoc> : ControllerBase where TDoc : MongoDoc
-    {
-        IMongoDocReader<TDoc> _reader;
-
-        public MongoController(IMongoDocReader<TDoc> reader)
-        {
-            _reader = reader;
-        }
-
-        [HttpGet]
-        public ActionResult<List<TDoc>> Get()
-        {
-            return _reader.Find(x => true);
-        }
-
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
 
     [Route("api/[controller]")]
     [ApiController]
     public class ChannelsController : MongoController<Channel>
     {
-        public ChannelsController(IMongoDocReader<Channel> reader) : base(reader)
+        public ChannelsController(IMongoDocReader<Channel> reader, IMongoDocWriter<Channel> writer) : base(reader, writer)
         { }
     }
 }
