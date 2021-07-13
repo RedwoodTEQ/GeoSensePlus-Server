4b05e04bcafe84eb0c4941adaea618474b78e7d7
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-13 22:21:51 +1000

    add database models and dbsets

--- GeoSensePlus.Data/ApplicationDbContext.cs
+++ GeoSensePlus.Data/ApplicationDbContext.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GeoSensePlus.Data.DatabaseModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeoSensePlus.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<AlarmEvent> AlarmEvents { get; set; }
+        public DbSet<BleEdge> BleEdges { get; set; }
+        public DbSet<BleTag> BleTags { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Floorplan> Floorplans { get; set; }
+        public DbSet<Gateway> Gateway { get; set; }
+        public DbSet<Geofence> Geofences { get; set; }
+        public DbSet<GpsTag> GpsTags { get; set; }
+        public DbSet<Target> Targets { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
         {
---
+++ GeoSensePlus.Data/DatabaseModels/AlarmEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoSensePlus.Data.DatabaseModels
+{
+    public class AlarmEventEntity
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Values:
+        /// 4: fatal
+        /// 3: failure or exception
+        /// 2: warning
+        /// 1: information with state change
+        /// 0: information without state change
+        /// </summary>
+        public string Severity { get; set; }
+
+        public string Source { get; set; }
+
+        /// <summary>
+        /// Lifecycle state: acknowledge, restore, clear, etc
+        /// </summary>
+        public string State { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class AlarmEvent
+    {
+    }
+}
