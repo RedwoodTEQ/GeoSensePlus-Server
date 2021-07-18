8d3b9bcce542ca6f0bc89ebdc9ff832aaaf32da1
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-19 03:42:59 +1000

    #2  add TopicsController

--- GeoSensePlus.Data/DatabaseModels/Topic.cs
+++ GeoSensePlus.Data/DatabaseModels/Topic.cs
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 
 namespace GeoSensePlus.Data.DatabaseModels
 {
-    public class TopicEntity : IIdAvailable<int>
+    public class TopicEntity
     {
-        public int TopicId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public int GetId()
-        {
-            return TopicId;
-        }
     }
 
     /// <summary>
     /// Subscription topic
     /// Used by MQTT or any other notification engines
     /// </summary>
-    public class Topic : TopicEntity
+    public class Topic : TopicEntity, IIdAvailable<int>
     {
+        public int TopicId { get; set; }
+
+        public int GetId()
+        {
+            return TopicId;
+        }
     }
 }
---
+++ GeoSensePlus.WebApi/Controllers/TopicsController.cs
@@ -0,0 +1,53 @@
+﻿using GeoSensePlus.Data.DatabaseModels;
+using GeoSensePlus.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoSensePlus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        IControllerUtil<Topic> _controllerUtil;
+
+        public TopicsController(IControllerUtil<Topic> controllerUtil)
+        {
+            _controllerUtil = controllerUtil;
+        }
+
+        [HttpGet]
+        public IEnumerable<Topic> Get()
+        {
+            return _controllerUtil.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Topic> Get(int id)
+        {
+            return _controllerUtil.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<TopicEntity> Post([FromBody] TopicEntity value)
+        {
+            var topic = new Topic { Name = value.Name, Description = value.Description };
+            return _controllerUtil.Post(topic);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] TargetEntity value)
+        {
+            var topic = new Topic { TopicId = id, Name = value.Name, Description = value.Description };
+            return _controllerUtil.Put(topic);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _controllerUtil.Delete(id);
+        }
+    }
+}
