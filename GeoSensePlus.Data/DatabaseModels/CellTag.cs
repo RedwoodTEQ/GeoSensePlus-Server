0c6660809357255c114c715e326d63ffaa9a75f4
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-18 18:09:25 +1000

    #2 add CellTagsController

--- GeoSensePlus.Data/DatabaseModels/CellTag.cs
+++ GeoSensePlus.Data/DatabaseModels/CellTag.cs
@@ -7,21 +7,22 @@ using System.Threading.Tasks;
 
 namespace GeoSensePlus.Data.DatabaseModels
 {
-    public class CellTagEntity : IIdAvailable<int>
+    public class CellTagEntity
     {
-        public int CellTagId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime TimeStamp { get; set; }
+    }
+
+    public class CellTag : CellTagEntity, IIdAvailable<int>
+    {
+        public Target Target { get; set; }
+
+        public int CellTagId { get; set; }
 
         public int GetId()
         {
             return CellTagId;
         }
-    }
-
-    public class CellTag : CellTagEntity
-    {
-        public Target Target { get; set; }
     }
 }
---
+++ GeoSensePlus.WebApi/Controllers/CellTagsController.cs
@@ -0,0 +1,54 @@
+﻿using GeoSensePlus.Data.DatabaseModels;
+using GeoSensePlus.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoSensePlus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CellTagsController : ControllerBase
+    {
+        IControllerUtil<CellTag> _controllerUtil;
+
+        public CellTagsController(IControllerUtil<CellTag> controllerUtil)
+        {
+            _controllerUtil = controllerUtil;
+        }
+
+        [HttpGet]
+        public IEnumerable<CellTag> Get()
+        {
+            return _controllerUtil.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CellTag> Get(int id)
+        {
+            return _controllerUtil.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<BuildingEntity> Post([FromBody] BuildingEntity value)
+        {
+            var celltag = new CellTag { Name = value.Name, Description = value.Description };
+            return _controllerUtil.Post(celltag);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] BuildingEntity value)
+        {
+            var celltag = new CellTag { CellTagId = id, Name = value.Name, Description = value.Description };
+            return _controllerUtil.Put(celltag);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _controllerUtil.Delete(id);
+        }
+
+    }
+}
