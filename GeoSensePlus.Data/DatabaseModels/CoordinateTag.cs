10d385045c2d5a28dc9fa38ed83dcc473b7fa7c4
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-18 23:33:50 +1000

    #2 add CoordinateTagsController

--- GeoSensePlus.Data/DatabaseModels/CoordinateTag.cs
+++ GeoSensePlus.Data/DatabaseModels/CoordinateTag.cs
@@ -4,22 +4,26 @@ using System;
 namespace GeoSensePlus.Data.DatabaseModels
 {
 
-    public class CoordinateTagEntity : IIdAvailable<int>
+    public class CoordinateTagEntity
     {
-        public int CoordinateTagId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
         public double AxisX { get; set; } = -1;
         public double AxisY { get; set; } = -1;
         public double AxisZ { get; set; } = -1;
         public DateTime TimeStamp { get; set; }
+    }
+
+    public class CoordinateTag : CoordinateTagEntity, IIdAvailable<int>
+    {
+        public Target Target { get; set; }
+
+        public int CoordinateTagId { get; set; }
 
         public int GetId()
         {
             return CoordinateTagId;
         }
-    }
-
-    public class CoordinateTag : CoordinateTagEntity
-    {
-        public Target Target { get; set; }
     }
 }
---
+++ GeoSensePlus.WebApi/Controllers/CoordinateTagsController.cs
@@ -0,0 +1,56 @@
+﻿using GeoSensePlus.Data.DatabaseModels;
+using GeoSensePlus.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoSensePlus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoordinateTagsController : ControllerBase
+    {
+        IControllerUtil<CoordinateTag> _controllerUtil;
+
+        public CoordinateTagsController(IControllerUtil<CoordinateTag> controllerUtil)
+        {
+            _controllerUtil = controllerUtil;
+        }
+
+        [HttpGet]
+        public IEnumerable<CoordinateTag> Get()
+        {
+            return _controllerUtil.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CoordinateTag> Get(int id)
+        {
+            return _controllerUtil.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<CoordinateTagEntity> Post([FromBody] CoordinateTagEntity value)
+        {
+            var tag = new CoordinateTag { Name = value.Name, Description = value.Description };
+            return _controllerUtil.Post(tag);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] CoordinateTagEntity value)
+        {
+            var tag = new CoordinateTag { CoordinateTagId = id, Name = value.Name, Description = value.Description };
+            return _controllerUtil.Put(tag);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _controllerUtil.Delete(id);
+        }
+    }
+}
