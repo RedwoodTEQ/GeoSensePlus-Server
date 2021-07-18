ba17452e9db8a3e2b9a1a43f89602f070c95e94b
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-19 03:21:05 +1000

    #2 add SensorsController

--- GeoSensePlus.Data/DatabaseModels/Sensor.cs
+++ GeoSensePlus.Data/DatabaseModels/Sensor.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace GeoSensePlus.Data.DatabaseModels
 {
-    public class SensorEntity : IIdAvailable<int>
+    /// <summary>
+    /// Represent a "field" in influxdb
+    /// </summary>
+    public class SensorEntity
     {
-        public int SensorId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
@@ -19,6 +21,12 @@ namespace GeoSensePlus.Data.DatabaseModels
         public string Type { get; set; }
 
         /// <summary>
+        /// Multiple lables are separated by ';'
+        /// Used as influxdb tags
+        /// </summary>
+        public string Labels { get; set; }
+
+        /// <summary>
         /// The latest reported value
         /// </summary>
         public double Value { get; set; }
@@ -26,17 +34,19 @@ namespace GeoSensePlus.Data.DatabaseModels
         public string Unit { get; set; }
 
         public DateTime TimeStamp { get; set; }
+    }
+
+    public class Sensor : SensorEntity, IIdAvailable<int>
+    {
+        //public Area CachedArea { get; set; }
+        public Measure Measure { get; set; }
+        public Edge Edge { get; set; }
+
+        public int SensorId { get; set; }
 
         public int GetId()
         {
             return SensorId;
         }
-    }
-
-    public class Sensor : SensorEntity
-    {
-        //public Area CachedArea { get; set; }
-        public Measure Measure { get; set; }
-        public Edge Edge { get; set; }
     }
 }
---
+++ GeoSensePlus.WebApi/Controllers/SensorsController.cs
@@ -0,0 +1,53 @@
+﻿using GeoSensePlus.Data.DatabaseModels;
+using GeoSensePlus.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoSensePlus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorsController : ControllerBase
+    {
+        IControllerUtil<Sensor> _controllerUtil;
+
+        public SensorsController(IControllerUtil<Sensor> controllerUtil)
+        {
+            _controllerUtil = controllerUtil;
+        }
+
+        [HttpGet]
+        public IEnumerable<Sensor> Get()
+        {
+            return _controllerUtil.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Sensor> Get(int id)
+        {
+            return _controllerUtil.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<SensorEntity> Post([FromBody] SensorEntity value)
+        {
+            var sensor = new Sensor { Name = value.Name, Description = value.Description, Labels = value.Labels };
+            return _controllerUtil.Post(sensor);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] SensorEntity value)
+        {
+            var sensor = new Sensor {SensorId = id, Name = value.Name, Description = value.Description, Labels = value.Labels };
+            return _controllerUtil.Put(sensor);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _controllerUtil.Delete(id);
+        }
+    }
+}
