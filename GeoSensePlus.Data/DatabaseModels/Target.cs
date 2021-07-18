7f72f6bea16671589d918132d2139c00b9c193e2
li-rongcheng <Li.Rongcheng@gmail.com>
2021-07-19 03:38:49 +1000

    #2 add TargetsController

--- GeoSensePlus.Data/DatabaseModels/Target.cs
+++ GeoSensePlus.Data/DatabaseModels/Target.cs
@@ -7,24 +7,25 @@ using System.Threading.Tasks;
 
 namespace GeoSensePlus.Data.DatabaseModels
 {
-    public class TargetEntity : IIdAvailable<int>
+    public class TargetEntity
     {
-        public int TargetId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public int GetId()
-        {
-            return TargetId;
-        }
     }
 
-    public class Target : TargetEntity
+    public class Target : TargetEntity, IIdAvailable<int>
     {
         //public Area CacheArea { get; set; }
 
         public List<CellTag> CellTags { get; set; } = new List<CellTag>();
         public List<CoordinateTag> CoordinateTags { get; set; } = new List<CoordinateTag>();
         public List<GpsTag> GpsTags { get; set; } = new List<GpsTag>();
+
+        public int TargetId { get; set; }
+
+        public int GetId()
+        {
+            return TargetId;
+        }
     }
 }
---
+++ GeoSensePlus.WebApi/Controllers/TargetsController.cs
@@ -0,0 +1,53 @@
+﻿using GeoSensePlus.Data.DatabaseModels;
+using GeoSensePlus.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoSensePlus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TargetsController : ControllerBase
+    {
+        IControllerUtil<Target> _controllerUtil;
+
+        public TargetsController(IControllerUtil<Target> controllerUtil)
+        {
+            _controllerUtil = controllerUtil;
+        }
+
+        [HttpGet]
+        public IEnumerable<Target> Get()
+        {
+            return _controllerUtil.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Target> Get(int id)
+        {
+            return _controllerUtil.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<TargetEntity> Post([FromBody] TargetEntity value)
+        {
+            var target = new Target { Name = value.Name, Description = value.Description };
+            return _controllerUtil.Post(target);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] TargetEntity value)
+        {
+            var target = new Target { TargetId = id, Name = value.Name, Description = value.Description };
+            return _controllerUtil.Put(target);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return _controllerUtil.Delete(id);
+        }
+    }
+}
