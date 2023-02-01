7a88240f31bd1b0b62283607608ccf8fe813a8a0
Ron <li.rongcheng@gmail.com>
2023-02-02 03:07:01 +1100

    Add UwbAnchorsController class

---
+++ GeoSensePlus.WebApi/Controllers/Locating/UwbAnchorsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoSensePlus.WebApi.Controllers.Locating
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UwbAnchorsController : ControllerBase
+    {
+    }
+}
