20a2167d3c15649adc70247bc44b10ea37aae716
Ron <li.rongcheng@gmail.com>
2023-02-02 02:27:20 +1100

    Remove obsolete project files

--- GeoSensePlus.Server/pack-project.bat
+++
@@ -1,6 +0,0 @@
-dotnet pack -c Release -o ../Publish/
-
-:: %1 should be "install" or "update"
-:: dotnet tool %1 -g GeoSensePlus.Server --add-source ./bin/NuPkg
-
-:: dotnet publish -c Release -o ./bin/Publish/GeoSensePlus.Cli
--- GeoSensePlus.Server/serve-dapr.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET APP_PORT=5200
-:: dapr run --app-id entry --dapr-http-port 3500
-:: localhost:3500/v1.0/invoke/geosenseplus-server/method/system/version
-dapr run --app-id geosenseplus-server --app-port %APP_PORT% -- dotnet watch run --urls http://localhost:%APP_PORT%
-@echo on
