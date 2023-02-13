23ae9a9f518ff8ccfe43eb0f2f1ab66ca4a2a74c
Ron <li.rongcheng@gmail.com>
2023-02-14 03:08:36 +1100

    Add service models

---
+++ GeoSensePlus.Core/ServiceModels/Consts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoSensePlus.Core.ServiceModels;
+static class Consts
+{
+    public const string unknown = nameof(unknown);
+
+    public const string uwb_anchor = nameof(uwb_anchor);
+
+    public const string cell_anchor = nameof(cell_anchor);
+
+    public const string gateway = nameof(gateway);
+}
---
+++ GeoSensePlus.Core/ServiceModels/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GeoSensePlus.Core.ServiceModels;
+
+public class UserInfo
+{
+    public string CognitoUserPoolId { get; set; }
+    public string CognitoUserName { get; set; }
+
+    /// <summary>
+    /// ASP.NET Core Identity user ID
+    /// </summary>
+    public string IdentityUserId { get; set; }
+}
