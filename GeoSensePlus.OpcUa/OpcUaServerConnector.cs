848edbc45489e6c8c99321d9ab5367fbf5d8f96e
cv <cynosure.veda@gmail.com>
2024-12-18 03:37:43 +1100

    feat: Add OpcUaServerConnector for managing OPC UA server connections and tag monitoring

---
+++ GeoSensePlus.OpcUa/OpcUaServerConnector.cs
@@ -0,0 +1,190 @@
+﻿using Opc.Ua;   // Install-Package OPCFoundation.NetStandard.Opc.Ua
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using System.Threading;
+
+namespace GeoSensePlus.OpcUa;
+
+public class OpcUaServerConnector
+{
+    public bool SecurityEnabled { get; set; }
+    public string MyApplicationName { get; set; }
+    public Session OpcUaSession { get; set; }
+    public string OpcUaNameSpace { get; set; }
+    public Dictionary<string, TagObject> TagList { get; set; }
+    private string DiscoveryUrl { get; set; }
+    public bool SessionRenewalRequired { get; set; }
+    public double SessionRenewalPeriodMins { get; set; }
+    public DateTime LastTimeSessionRenewed { get; set; }
+    public DateTime LastTimeOPCServerFoundAlive { get; set; }
+    public bool ClassDisposing { get; set; }
+    public bool InitialisationCompleted { get; set; }
+
+
+    // In some OPC UA servers, the session can be closed from the Server side, so its better to 
+    // allow the class to reinitiate session periodically
+    private Thread RenewerThread { get; set; }
+    private CancellationTokenSource tokenSource = new();
+
+    public OpcUaServerConnector(string discoveryUrl, Dictionary<string, TagObject> taglist, bool sessionrenewalRequired, double sessionRenewalMinutes, string nameSpace)
+    {
+        DiscoveryUrl = discoveryUrl;
+        MyApplicationName = "MyApplication";
+        TagList = taglist;
+        SessionRenewalRequired = sessionrenewalRequired;
+        SessionRenewalPeriodMins = sessionRenewalMinutes;
+        OpcUaNameSpace = nameSpace;
+        LastTimeOPCServerFoundAlive = DateTime.Now;
+        InitializeOPCUAClient();
+
+        if (SessionRenewalRequired)
+        {
+            LastTimeSessionRenewed = DateTime.Now;
+            RenewerThread = new(() => RenewSessionThread(tokenSource.Token));
+            RenewerThread.Start();
+        }
+    }
+
+    ~OpcUaServerConnector()
+    {
+        ClassDisposing = true;
+        try
+        {
+            OpcUaSession.Close();
+            OpcUaSession.Dispose();
+
+            // Stop thread
+            tokenSource.Cancel();
+            RenewerThread.Join(); // wait thread to finish
+            tokenSource.Dispose();
+
+        }
+        catch { }
+    }
+
+    private void RenewSessionThread(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested && !ClassDisposing)
+        {
+            if ((DateTime.Now - LastTimeSessionRenewed).TotalMinutes > SessionRenewalPeriodMins
+            || (DateTime.Now - LastTimeOPCServerFoundAlive).TotalSeconds > 60)
+            {
+                Console.WriteLine("Renewing Session");
+                try
+                {
+                    OpcUaSession.Close();
+                    OpcUaSession.Dispose();
+                }
+                catch { }
+                InitializeOPCUAClient();
+                LastTimeSessionRenewed = DateTime.Now;
+            }
+            Thread.Sleep(2000);
+        }
+    }
+
+    public void InitializeOPCUAClient()
+    {
+        //Console.WriteLine("Step 1 - Create application configuration and certificate.");
+        var config = new ApplicationConfiguration()
+        {
+            ApplicationName = MyApplicationName,
+            //ApplicationUri = Utils.Format(@"urn:{0}:" + MyApplicationName + "", ServerAddress),
+            ApplicationType = ApplicationType.Client,
+            SecurityConfiguration = new SecurityConfiguration
+            {
+                //ApplicationCertificate = new CertificateIdentifier { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\MachineDefault", SubjectName = Utils.Format(@"CN={0}, DC={1}", MyApplicationName, ServerAddress) },
+                ApplicationCertificate = new CertificateIdentifier { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\MachineDefault", SubjectName = "TestSubject1" },
+                TrustedIssuerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Certificate Authorities" },
+                TrustedPeerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Applications" },
+                RejectedCertificateStore = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\RejectedCertificates" },
+                AutoAcceptUntrustedCertificates = true,
+                AddAppCertToTrustedStore = true
+            },
+            TransportConfigurations = new TransportConfigurationCollection(),
+            TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+            ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+            TraceConfiguration = new TraceConfiguration()
+        };
+        config.Validate(ApplicationType.Client).GetAwaiter().GetResult();
+        if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+        {
+            config.CertificateValidator.CertificateValidation += (s, e) =>
+            {
+                e.Accept = e.Error.StatusCode == StatusCodes.BadCertificateUntrusted;
+            };
+        }
+
+        var application = new ApplicationInstance
+        {
+            ApplicationName = MyApplicationName,
+            ApplicationType = ApplicationType.Client,
+            ApplicationConfiguration = config
+        };
+        application.CheckApplicationInstanceCertificate(false, 2048).GetAwaiter().GetResult();
+
+
+        var selectedEndpoint = CoreClientUtils.SelectEndpoint(DiscoveryUrl, useSecurity: SecurityEnabled, discoverTimeout: 15000);
+
+        OpcUaSession = Session.Create(config, new ConfiguredEndpoint(null, selectedEndpoint, EndpointConfiguration.Create(config)), false, "", 60000, null, null).GetAwaiter().GetResult();
+
+        var subscription = new Subscription(OpcUaSession.DefaultSubscription) { PublishingInterval = 1000 };
+
+        var list = new List<MonitoredItem> { };
+
+
+        // TODO: refactor this list logic
+        list.Add(new MonitoredItem(subscription.DefaultItem) { DisplayName = "RLTest1", StartNodeId = "ns=3;i=1001" });
+
+        foreach (KeyValuePair<string, TagObject> td in TagList)
+            list.Add(new MonitoredItem(subscription.DefaultItem) { DisplayName = td.Value.DisplayName, StartNodeId = "ns=" + OpcUaNameSpace + ";s=" + td.Value.NodeID });
+
+        list.ForEach(i => i.Notification += OnTagValueChange);
+        subscription.AddItems(list);
+
+        OpcUaSession.AddSubscription(subscription);
+
+
+        subscription.Create();
+    }
+
+    public void OnTagValueChange(MonitoredItem item, MonitoredItemNotificationEventArgs e)
+    {
+
+        foreach (var value in item.DequeueValues())
+        {
+            if (item.DisplayName == "ServerStatusCurrentTime")
+            {
+                LastTimeOPCServerFoundAlive = value.SourceTimestamp.ToLocalTime();
+            }
+            else
+            {
+                if (value.Value != null)
+                {
+                    Console.WriteLine("{0}: {1}, {2}, {3}", item.DisplayName, value.Value.ToString(), value.SourceTimestamp.ToLocalTime(), value.StatusCode);
+                    System.Diagnostics.Debug.WriteLine("{0}: {1}, {2}, {3}", item.DisplayName, value.Value.ToString(), value.SourceTimestamp.ToLocalTime(), value.StatusCode);
+                }
+                else
+                    Console.WriteLine("{0}: {1}, {2}, {3}", item.DisplayName, "Null Value", value.SourceTimestamp, value.StatusCode);
+
+                if (TagList.ContainsKey(item.DisplayName))
+                {
+                    if (value.Value != null)
+                    {
+                        TagList[item.DisplayName].LastGoodValue = value.Value.ToString();
+                        TagList[item.DisplayName].CurrentValue = value.Value.ToString();
+                        TagList[item.DisplayName].LastUpdatedTime = DateTime.Now;
+                        TagList[item.DisplayName].LastSourceTimeStamp = value.SourceTimestamp.ToLocalTime();
+                        TagList[item.DisplayName].StatusCode = value.StatusCode.ToString();
+                    }
+                    else
+                    {
+                        TagList[item.DisplayName].StatusCode = value.StatusCode.ToString();
+                        TagList[item.DisplayName].CurrentValue = null;
+                    }
+                }
+            }
+        }
+        InitialisationCompleted = true;
+    }
+}
