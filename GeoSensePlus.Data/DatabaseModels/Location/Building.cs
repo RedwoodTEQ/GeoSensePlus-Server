bd5574101837d03d4dc1098406742c26109faf61
cv <cynosure.veda@gmail.com>
2025-07-11 03:35:45 +1000

    feat: add site relationship to building model

--- GeoSensePlus.Data/DatabaseModels/Location/Building.cs
+++ GeoSensePlus.Data/DatabaseModels/Location/Building.cs
@@ -9,10 +9,13 @@ namespace GeoSensePlus.Data.DatabaseModels.Location;
 
 public class BuildingEntity : NamedEntity<int>
 {
+    [Column("site_id")]
+    public int? SiteId { get; set; } // Foreign key to Site
 }
 
 [Table("building", Schema = SchemaNames.location)]
 public class Building : BuildingEntity
 {
+    public Site Site { get; set; }
     public List<FloorPlan> FloorPlans { get; set; } = new List<FloorPlan>();
 }
