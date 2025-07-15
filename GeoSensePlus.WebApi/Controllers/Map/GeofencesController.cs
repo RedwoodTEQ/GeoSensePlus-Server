911ba7f13093df3d4c618d466e491f1d08d9351b
cv <cynosure.veda@gmail.com>
2025-07-16 02:07:29 +1000

    rename zone to area and change and rename namespace

--- GeoSensePlus.WebApi/Controllers/Map/GeofencesController.cs
+++ GeoSensePlus.WebApi/Controllers/Map/GeofencesController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace GeoSensePlus.WebApi.Controllers.Positioning
+namespace GeoSensePlus.WebApi.Controllers.Map
 {
     [Route("api/[controller]")]
     [ApiController]
---
+++ GeoSensePlus.WebApi/Controllers/Messaging/PointsController.cs
@@ -0,0 +1,32 @@
+﻿using DnsClient.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GeoSensePlus.WebApi.Controllers.Messaging;
+
+/// <summary>
+/// UNS points controller.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class PointsController : ControllerBase
+{
+    ILogger<PointsController> _logger;
+    public PointsController(ILogger<PointsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("{*path}")]
+    public IActionResult Get(string path)
+    {
+        return Ok($"Received request for UNS with path: {path}");
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] object data)
+    {
+        _logger.LogInformation("Received POST request for UNS with data: {Data}", data);
+        return Ok($"Received POST request for UNS, data: {data.ToString()}");
+    }
+}
